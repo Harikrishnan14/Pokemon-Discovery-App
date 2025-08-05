dc019b02627923c483dccae6eb80fb365c7599e0
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-08-05 21:26:58 +0530

    Responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,17 @@ const Header = ({ savedCount }) => {
 
     return (
         <div className='p-6'>
-            <h2 className='font-bold text-3xl text-center mb-2'>🔥 Pokemon Collection App</h2>
-            <p className='font-normal text-lg text-center mb-3'>Discover, collect, and organize our favorite Pokemon!</p>
+            <h2 className='font-bold md:text-3xl text-xl text-center mb-2'>🔥 Pokemon Collection App</h2>
+            <p className='font-normal md:text-lg text-sm text-center mb-3'>Discover, collect, and organize our favorite Pokemon!</p>
             <div className='flex items-center justify-center gap-6'>
                 <button
-                    className='px-5 py-2 bg-purple-700 rounded-full font-bold text-white hover:cursor-pointer hover:bg-purple-500 ease-linear transition'
+                    className='px-5 py-2 bg-purple-700 rounded-full text-xs md:text-base font-bold text-white hover:cursor-pointer hover:bg-purple-500 ease-linear transition'
                     onClick={() => navigate('/')}
                 >
                     🔍 Discover Pokemon
                 </button>
                 <button
-                    className='px-5 py-2 bg-indigo-400 rounded-full font-bold text-white hover:cursor-pointer hover:bg-indigo-600 ease-linear transition'
+                    className='px-5 py-2 bg-indigo-400 rounded-full text-xs md:text-base font-bold text-white hover:cursor-pointer hover:bg-indigo-600 ease-linear transition'
                     onClick={() => navigate('/collections')}
                 >
                     📚 My Collection {savedCount > 0 && `(${savedCount})`}
