ec2699ed2528a357b40c1b54b5a0816b7016e8de
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-08-05 18:12:43 +0530

    Routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from './Layouts/MainLayout';
+import Home from './pages/Home';
+import MyCollection from './pages/MyCollection';
 
 function App() {
   return (
-    <h1 class="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="collections" element={<MyCollection />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
