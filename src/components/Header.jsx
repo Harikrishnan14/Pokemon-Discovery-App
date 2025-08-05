d5faabf30534bd7f10440b1d290507da1a49c5d0
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-08-05 18:25:31 +0530

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+    const navigate = useNavigate()
     return (
-        <div>Header</div>
+        <div className='p-6'>
+            <h2 className='font-bold text-3xl text-center mb-2'>🔥 Pokemon Collection App</h2>
+            <p className='font-normal text-lg text-center mb-3'>Discover, collect, and organize our favorite Pokemon!</p>
+            <div className='flex items-center justify-center gap-6'>
+                <button
+                    className='px-5 py-2 bg-violet-400 rounded-full font-bold text-white hover:cursor-pointer hover:bg-violet-600 ease-linear transition'
+                    onClick={() => navigate('/')}
+                >
+                    🔍 Discover Pokemon
+                </button>
+                <button
+                    className='px-5 py-2 bg-blue-400 rounded-full font-bold text-white hover:cursor-pointer hover:bg-blue-600 ease-linear transition'
+                    onClick={() => navigate('/collections')}
+                >
+                    📚 My Collection (3)
+                </button>
+            </div>
+        </div>
     )
 }
 
